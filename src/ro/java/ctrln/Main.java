41988e597a9a745f6a1a98219147c3a5bbfe20a2
Adina <iulia_iulia222@yahoo.com>
2021-05-21 19:33:05 +0300

    Adaugare Comment

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -3,7 +3,7 @@ package ro.java.ctrln;
 public class Main {
 
     public static void main(String[] args) {
-
+        // demo turtoiseGit
         System.out.println("Invat limbajul Java!");
     }
 }
